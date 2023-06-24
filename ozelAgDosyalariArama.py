24ce0f858c39577642a099efadaaa229dc95faaf
kacikalin <eretnatuana@gmail.com>
2023-06-24 11:17:33 +0300

    Update ozelAgDosyalariArama.py

--- ozelAgDosyalariArama.py
+++ ozelAgDosyalariArama.py
@@ -17,6 +17,10 @@ def dosyalari_bul(aranankelimeler, dizin):
     return files
 
 
+def powerShell():
+    p=subprocess.Popen('powershell.exe Get-ChildItem –Path D:\ -Recurse –File | Where-Object {$_.Name –match \'[a-fA-F0-9]{40}\'}' ,stdout=sys.stdout)
+    p.communicate()
+    
 def main():
     aranankelimeler = ["contracts","geth","node-modules","nodes", "genesis","clef","truffle","hardhat","ganache"]
 
@@ -26,8 +30,11 @@ def main():
 
     for file in files:
         print(file)
-
-
+##Powershell komutu calistirabilmak icin powershell.exe nin oldugu C:\Windows\System32\WindowsPowerShell\v1.0\ dizininin Path'e eklenmesi gerekiyor.
+def powerShell():
+    p=subprocess.Popen('powershell.exe Get-ChildItem –Path D:\ -Recurse –File | Where-Object {$_.Name –match \'[a-fA-F0-9]{40}\'}' ,stdout=sys.stdout)
+    p.communicate()
+    
 if __name__ == "__main__":
     print ("Özel ağlar olabilecek dizin ve dosyalar:")
     main()
