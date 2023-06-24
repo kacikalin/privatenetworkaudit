6959717c16e0864adb67876683ba51213d6eddf0
kacikalin <eretnatuana@gmail.com>
2023-06-24 07:09:25 +0300

    Create ozelAgDosyalariArama.py

---
+++ ozelAgDosyalariArama.py
@@ -0,0 +1,32 @@
+import os
+
+import re
+
+
+def dosyalari_bul(aranankelimeler, dizin):
+
+    files = []
+
+    for root, directories, filenames in os.walk(dizin):
+
+        for filename in filenames:
+
+            if any(word in filename for word in aranankelimeler):
+                files.append(os.path.join(root, filename))
+
+    return files
+
+
+def main():
+    aranankelimeler = ["contracts","geth","node-modules","nodes", "genesis","clef","truffle","hardhat","ganache"]
+
+    dizin = "C:\\OrnekDizin"
+
+    files = dosyalari_bul(aranankelimeler, dizin)
+
+    for file in files:
+        print(file)
+
+
+if __name__ == "__main__":
+    main()
